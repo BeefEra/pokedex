a51392bd7258cc726cdbe329c27e75a5569bd1d8
BeefEra <zachboiss1@gmail.com>
2023-02-26 06:53:28 -0600

    Created associations and validations for Pokemon and Type models

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,6 @@
 class Pokemon < ApplicationRecord
+  has_many :pokemon_types, dependent: :destroy
+  has_many :types, through: :pokemon_types
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,2 +1,6 @@
 class Type < ApplicationRecord
+  has_many :pokemon_types, dependent: :destroy
+  has_many :pokemons, through: :pokemon_types
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
