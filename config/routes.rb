8dbf922685faa6b26a109ff50f5f0e144ea74cf7
BeefEra <zachboiss1@gmail.com>
2023-02-27 01:29:50 -0600

    added columns and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,5 @@ Rails.application.routes.draw do
   resources :abilities
   resources :moves
   resources :types
-  resources :pokemons
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :pokemons, only: %i[index show]
 end
